dd2eaf6ddfdfb1013a09e818692c977eb102c151
jesse <goosetherumfoodle@users.noreply.github.com>
2018-11-03 14:20:13 -0400

    extracts call respose text

--- library/Example.hs
+++ library/Example.hs
@@ -69,6 +69,7 @@ module Example where
 
 import Prelude hiding (until)
 
+import Test.QuickCheck (generate, elements)
 import Data.Foldable (asum)
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -1275,7 +1276,11 @@ server = twilMsg
           performCmd UnrecognizedNumber
 
     twilCall :: TwilioCallData -> Handler TwilioCallResponse
-    twilCall inMsg = liftIO (print inMsg) >> pure (twimlCall bob)
+    twilCall inMsg = do
+      liftIO (print inMsg)
+      responses <- liftIO loadCallResponse
+      response <- liftIO $ randItem responses
+      liftIO . pure . twimlCall $ response
 
 twimlCall :: Text -> TwilioCallResponse
 twimlCall = TCallResp
@@ -1450,60 +1455,10 @@ Available commands:
 "shifts": display 7 days worth of unclaimed shifts.
 "suspend (N)": Suspend for number of days (defaults to end of the shift-week)|]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+randItem :: [a] -> IO a
+randItem = generate . elements
 
 -- TODO: the limit on texts in a <Say> twiml node is 4,096.
 -- this text is currently 4652
-bob :: Text
-bob =  "In astronomy, \"revolution\" refers to a return to the same place. For the left it seems to mean about the same. Leftism is literally reactionary. Just as generals prepare to fight the last war, leftists incite the last revolution. They welcome it because they know it failed. They are vanguardists because they are always behind the times. Like all leaders, leftists are least obnoxious when following their following, but in certain crises they step to the fore to make the system work. If the left/right metaphor has any meaning, it can only be that the left is to the left of the same thing the right is to the right of. But what if revolution means stepping out of line? If there were no right, the left would have to invent it — and it often has. (Examples: Calculated hysteria over Nazis and the K K K which awards these wimpy slugs the notoriety they need; or lowest-common-denunciation of the Moral Majority obviating unmannerly attacks on the real sources of moralist tyranny — the family, religion in general, and the work-ethic espoused by leftists and Christians alike.) The right likewise needs the left: its operational definition is always anti-communism, variously drecked-out. Thus left and right presuppose and re-create each other. One bad thing about bad times is that they make opposition too easy, as (for instance) the current economic crisis gets shoehorned into archaic Marxist, populist or syndicalist categories. The left thereby positions itself to fulfill its historic role as reformer of those incidental (albeit agonizing) evils which, properly attended to, conceal the system's essential inequities: hierarchy, moralism, bureaucracy, wage-labor, monogamy, government, money. (How can Marxism ever be more than capitals most sophisticated way of thinking about itself?) Consider the acknowledged epicenter of the current crisis. work. Unemployment is a bad thing. But it doesn't follow, outside of righto-leftist dogma, that employment is a good thing. It is not. The \"right to work,\" arguably an appropriate slogan in 1848, is obsolete in 1982. People don't need work. What we need is satisfaction of subsistence requirements, on the one hand, and opportunities for creative, convivial, educative, diverse, passionate activity on the other. Twenty years ago the Goodman brothers guessed that 5% of the labor then expended would meet minimum survival needs, a figure which must be lower today; obviously entire so-called industries serve nothing but the predatory purposes of commerce and coercion. That's an ample infrastructure to play with in creating a world of freedom, community and pleasure where \"production\" of use-values is \"consumption\" of free gratifying activity. Transforming work into play is a project for a proletariat that refuses that condition, not for leftists left with nothing to lead. Pragmatism, as is obvious from a glance at its works, is a delusive snare. Utopia is sheer common sense. The choice between \"full employment\" and unemployment — the choice that left and right collaborate to confine us to — is the choice between the Gulag and the gutter. No wonder that after all these years a stifled and suffering populace is weary of the democratic lie. There are less and less people who want to work, even among those who rightly fear unemployment, and more and more people who want to work wonders. By all means let's agitate for handouts, tax cuts, freebies, bread and circuses — why not bite the hand that feeds you? the flavor is excellent — but without illusions. The surrational kernel of truth in the mystical Marxist shell is this: the \"working class\" is the legendary \"revolutionary agent\": but only if, by not working, it abolishes class. Perennial \"organizers,\" leftists don't understand that the workers have already been definitively \"organized\" by, and can only be organized for — their bosses. \"Activism\" is idiocy if it enriches and empowers our enemies. Leftism, that parasite for sore eyes, dreads the outbreak of a Wilhelm Reichstag fire which will consume its parties and unions along with the corporations and armies and churches currently controlled by its ostensible opposite. Nowadays you have to be odd to get even. Greylife leftism, with its checklists of obligatory antagonisms (to this-ism, that-ism and the other-ism: everything but leftism) is devoid of all humor and imagination: hence it can stage only coups, not revolutions, which change lies but not life. But the urge to create is also a destructive urge. One more effort, leftists, if you would be revolutionaries! If you're not revolting against work, you're working against revolt."
+loadCallResponse :: IO [Text]
+loadCallResponse = decodeFileThrow . (<> "/call_responses.yml") =<< configDir
