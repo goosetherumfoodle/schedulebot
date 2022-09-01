b2cf8bade9e53f58200b2ca168487766487d9011
goosetherumfoodle <13107006+goosetherumfoodle@users.noreply.github.com>
2022-09-01 12:38:31 -0400

    adds db, that won't compile

--- default.nix
+++ default.nix
@@ -8,6 +8,8 @@ let
         meta.broken = false;
       });
       servant-xml = super.servant-xml.overrideAttrs (old: { meta.broken = false; });
+      project-m36 = import (fetchTarball "https://github.com/agentm/project-m36/archive/refs/tags/v0.9.4.tar.gz");
+      # streamly = import (fetchTarball "https://github.com/composewell/streamly/releases/download/v0.7.3/streamly-0.7.3.tar.gz");
       xmlbf-xeno = super.xmlbf-xeno.overrideAttrs (old: { meta.broken = false; });
     };
   };
---
+++ library/Bot/Database.hs
@@ -0,0 +1,3 @@
+module Database where
+
+import ProjectM36.Client ()
